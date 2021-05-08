eb2658a38ac9d5f2c4e88e33541aafd1b23e8a02
Ryan <cybervoid41@gmail.com>
2021-05-08 07:07:03 -0600

    Update TokenService

--- SCLFCrew/Application/Services/TokenService.cs
+++ SCLFCrew/Application/Services/TokenService.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
 using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
@@ -30,6 +31,8 @@ namespace SCLFCrew.Application.Services
             };
 
             var roles = await _userManager.GetRolesAsync(user);
+
+            claims.AddRange(roles.Select(role => new Claim(ClaimTypes.Role, role)));
 
             var creds = new SigningCredentials(_key, SecurityAlgorithms.HmacSha512Signature);
             var tokenDescriptor = new SecurityTokenDescriptor()
